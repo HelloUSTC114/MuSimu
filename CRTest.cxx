c94f02c4a1c6b76e7d2758d2a5adee752ab29d1a
HelloUSTC114 <liangzheng1021@163.com>
2022-09-19 01:37:19 +0800

    debug crtest, make it stable while closing window

--- CRTest.cxx
+++ CRTest.cxx
@@ -1,12 +1,12 @@
 /* Cosmic-ray Test Simulation Platform
-*
-*  Reference : example/basic/B5
-*
-*  Author : ytwu (torrence@mail.ustc.edu.cn)
-*
-*  Created : 2017.02.17
-*
-*/
+ *
+ *  Reference : example/basic/B5
+ *
+ *  Author : ytwu (torrence@mail.ustc.edu.cn)
+ *
+ *  Created : 2017.02.17
+ *
+ */
 
 /* file : CRTest.cxx
 ** desc.: Main Program for CRTest
@@ -27,78 +27,88 @@
 #include "G4VModularPhysicsList.hh"
 #include "G4StepLimiterPhysics.hh"
 
+#include "G4RootAnalysisManager.hh"
+
 #include "globals.hh"
 #include "G4ios.hh"
-#include<cstdlib>
+#include <cstdlib>
+#include <fstream>
 
-int main (int argc, char** argv){
+int main(int argc, char **argv)
+{
     // Handle Arguments
-	Argument args;
-	if(!args.Build(argc, argv))    //Build(argc,argv)处理命令行参数，将执行程序，gdml读出
-	{
-		args.Usage();              //G4cout << " Usage : ./CRTest [.gdml] [.mac] [output] [seed]"<< G4endl;
-		return -1;
-	}
+    Argument args;
+    if (!args.Build(argc, argv)) // Build(argc,argv)处理命令行参数，将执行程序，gdml读出
+    {
+        args.Usage(); // G4cout << " Usage : ./CRTest [.gdml] [.mac] [output] [seed]"<< G4endl;
+        return -1;
+    }
     // UI Session   //定义是否使用UI界面，还是直接命令行跑
-    G4UIExecutive* ui = NULL;
+    G4UIExecutive *ui = NULL;
     if (args.Ui())
-        ui = new G4UIExecutive(argc, argv);  
+        ui = new G4UIExecutive(argc, argv);
     // Random
     G4Random::setTheEngine(new CLHEP::RanecuEngine);
-    G4Random::setTheSeed(args.RndSeed(),3);
+    G4Random::setTheSeed(args.RndSeed(), 3);
     // Run manager
-    G4RunManager* runManager = new G4RunManager;
+    G4RunManager *runManager = new G4RunManager;
     // Welcome info.
     G4cout << G4endl << "[***] WELCOME - CRTest Start!" << G4endl << G4endl;
-	// Print Arguments & Configuration Info.
-	args.Print();
-	// User defined classes 
-        // Detector Construction
+    // Print Arguments & Configuration Info.
+    args.Print();
+    // User defined classes
+    // Detector Construction
     runManager->SetUserInitialization(new GdmlConstruction(args.Gdml()));
-		// Physics List
+    // Physics List
     runManager->SetUserInitialization(new PhysicsList);
-		// User Actions
+    // User Actions
     runManager->SetUserInitialization(new ActionRegister);
-		// User Messenger
-    SysMessenger* messenger = new SysMessenger(runManager);
-    
-	// Visualization Manager
-		// TODO : ADD verbosiry option and if/else by arg. mode
-    G4VisManager* visManager = NULL;
-	if(args.Vis()){
-		visManager = new G4VisExecutive("error");
-		visManager->Initialize();
-	}
+    // User Messenger
+    SysMessenger *messenger = new SysMessenger(runManager);
+
+    // Visualization Manager
+    // TODO : ADD verbosiry option and if/else by arg. mode
+    G4VisManager *visManager = NULL;
+    if (args.Vis())
+    {
+        visManager = new G4VisExecutive("error");
+        visManager->Initialize();
+    }
     // UIManager
-    G4UImanager* uiManager = G4UImanager::GetUIpointer();
-	if( !args.Vis()){
+    G4UImanager *uiManager = G4UImanager::GetUIpointer();
+    if (!args.Vis())
+    {
         // Set output .root file name from argument
         G4String command = "/analysis/setFileName ";
-		uiManager->ApplyCommand(command+args.Root());
+        uiManager->ApplyCommand(command + args.Root());
         // Execute macro from argument
         command = "/control/execute ";
-		uiManager->ApplyCommand(command+args.Macro());
+        uiManager->ApplyCommand(command + args.Macro());
     }
-	else{
-		// Execute Initialization Macro by UIManager
-		uiManager->ApplyCommand("/control/execute ./mac/init.mac");
-		if(args.Ui()){
-			if(ui && ui->IsGUI())
-				uiManager->ApplyCommand("/control/execute ./mac/gui.mac");
-			// Start Session
-			ui->SessionStart();
-		}
-	}// mode case
-    
-    // delete
-	G4cout << G4endl << "[-] INFO - CRTest come to QUIT state." << G4endl;
-	if(ui) delete ui;
-	if(visManager)	delete visManager;
-	delete messenger;
+    else
+    {
+        // Execute Initialization Macro by UIManager
+        uiManager->ApplyCommand("/control/execute ./mac/init.mac");
+        if (args.Ui())
+        {
+            if (ui && ui->IsGUI())
+                uiManager->ApplyCommand("/control/execute ./mac/gui.mac");
+            // Start Session
+            ui->SessionStart();
+        }
+    } // mode case
+
+    // delete: After closing ui manager, this information will not be output.
+    // G4cout << G4endl << "[-] INFO - CRTest come to QUIT state." << G4endl;
+    if (ui)
+        delete ui;
+    if (visManager)
+        delete visManager;
+    delete messenger;
     delete runManager;
-    
-	// Exit info.
-    G4cout << G4endl << "[***] All is well - CRTest Exit." << G4endl << G4endl;
-    
-	return 0;
+
+    // Exit info.
+    // G4cout << G4endl << "[***] All is well - CRTest Exit." << G4endl << G4endl;
+
+    return 0;
 }
